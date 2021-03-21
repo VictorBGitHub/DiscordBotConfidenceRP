0d12c9f5b4facb64ae4e313bc8e8ea3a9fce4459
NitoGitHub <62777329+NitoGitHub@users.noreply.github.com>
2021-03-21 14:24:10 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -38,5 +38,16 @@ client.on("message", msg => {
       msg.channel.send("connect 188.40.16.78:30686");
   }
 
+
+  if (msg.member.hasPermission("ADMINISTRATOR")){
+    if (msg.content.startsWith(prefix == "boot")){
+        msg.channel.send("Le serveur vient de reboot, le staff vous souhaite un bon jeu !")
+        .then(msg => {
+          msg.delete({ timeout: 20000 /*time unitl delete in milliseconds*/});
+                })
+          .catch(/*Your Error handling if the Message isn't returned, sent, etc.*/);
+    }
+  }
+
   
 })
